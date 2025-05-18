dbb6e5b1fe4b9a336dbe765c5a28f89dee9394af
baejinwoo <qowlsdn8007@gmail.com>
2025-05-19 00:18:59 +0900

    Chore: Introduce typeORM and mongodb in auth, event server
    Configuration for use typeorm
    - set db_url, db_name for env variables
    - set container for docker-compose.yaml

--- apps/event/src/app.module.ts
+++ apps/event/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
@@ -9,6 +10,17 @@ import { AppService } from './app.service'
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get('DB_URL'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([]), // Add entities
   ],
   controllers: [AppController],
   providers: [AppService],
