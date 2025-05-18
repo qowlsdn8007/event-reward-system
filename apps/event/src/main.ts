811137e7c973210058188632aeb69310f7471b65
Jinwoo Bae <baejinwoo@jumpcloudui-MacBookPro.local>
2025-05-18 23:09:03 +0900

    Chore: Introduce event server and set config for msa
    Set Skeleton server of nestjs microservice node

---
+++ apps/event/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule)
+  const configService = appContext.get(ConfigService)
+
+  const host = configService.get('EVENT_SERVICE_HOST') ?? 'localhost'
+  const port = configService.get('DEFAULT_PORT') ?? 3002
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host,
+        port,
+      },
+    },
+  )
+  app.listen()
+}
+bootstrap()
